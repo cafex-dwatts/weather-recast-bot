12a9a92fd6556230bc8f905e10c2137366639720
cafex-dwatts <cafex-dwatts@users.noreply.github.com>
2017-08-07 11:59:11 +0100

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,65 +1,42 @@
+
 const recastai = require('recastai').default
 const client = new recastai(process.env.REQUEST_TOKEN)
 const request = require('request')
 
+const weatherApiKey = "&appid=" + process.env.WEATHER_TOKEN
+const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q="
+
 const replyMessage = (message, text, res) => {
-  const recastaiReq = new recastai.request(process.env.REQUEST_TOKEN, process.env.LANGUAGE)
-  const content = (message ? message.content : text)
+    const recastaiReq = new recastai.request(process.env.REQUEST_TOKEN, process.env.LANGUAGE)
+    const content = (message ? message.content : text)
 
-  recastaiReq.analyseText(content)
-    .then(recastaiRes => {
-      const intent = recastaiRes.intent()
-      console.log(intent)
+    recastaiReq.analyseText(content).then(recastaiRes => {
+        const intent = recastaiRes.intent()
+        console.log(intent)
 
-      if (intent && intent.slug === 'greetings') {
-        const reply = {
-          type: 'quickReplies',
-          content: {
-            title: 'Hi! What can I do for you?',
-            buttons: [
-              {
-                title: 'Chuck Norris fact',
-                value: 'Tell me a joke',
-              },
-              {
-                title: 'Goodbye',
-                value: 'Goodbye',
-              },
-            ],
-          },
+        if (intent && intent.slug === 'weather') {
+            request( weatherUrl + recastaiRes.entities.hasOwnProperty('location') +  weatherApiKey, (_err, _res, body) => {
+                body = JSON.parse(body)
+                const content = body.value
+
+                return message ? message.reply({ type: 'text', content }).then() : res.send({ reply: content })
+            })
         }
 
-        return message ? message.reply([reply]) : res.json({ reply: 'Hi, what can I do for you? :-)' })
-      }
+        if (intent && intent.slug === 'greetings') {
+            const reply = {
+                type: 'quickReplies',
+                content: {
+                    title: 'Hi! What can I do for you?',
+                    buttons: [
+                        { title: 'Chuck Norris fact', value: 'Tell me a joke' },
+                        { title: 'Goodbye', value: 'Goodbye' },
+                    ],
+                },
+            }
 
-      if (recastaiRes.entities.hasOwnProperty('category')) {
-        request('https://api.chucknorris.io/jokes/categories', (_err, _res, body) => {
-          const response = JSON.parse(body)
-          const category = recastaiRes.entities.category[0].value
-          if (response.indexOf(category)) {
-            request(`https://api.chucknorris.io/jokes/random?category=${category}`, (_err, _res, body) => {
-              body = JSON.parse(body)
-              const content = body.value
-
-              return message ? message.reply([{ type: 'text', content }]).then() : res.send({ reply: content })
-            })
-          } else {
-            request('https://api.chucknorris.io/jokes/categories', (_err, _res, body) => {
-              body = JSON.parse(body)
-              const content = `Sorry, I only know about these categories: ${body.join(', ')}.`
-
-              return message ? message.reply([{ type: 'text', content }]).then() : res.send({ reply: content })
-            })
-          }
-        })
-      } else {
-        request('https://api.chucknorris.io/jokes/random', (_err, _res, body) => {
-          body = JSON.parse(body)
-          const content = body.value
-
-          return message ? message.reply({ type: 'text', content }).then() : res.send({ reply: content })
-        })
-      }
+            return message ? message.reply([reply]) : res.json({ reply: 'Hi, what can I do for you? :-)' })
+        }
     })
 }
 
