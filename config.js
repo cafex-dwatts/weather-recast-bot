9a895d1dcc6c387823b7a2e4ce7b001276a08ac3
cafex-dwatts <cafex-dwatts@users.noreply.github.com>
2017-08-07 11:58:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 process.env.PORT = '5000'
 process.env.REQUEST_TOKEN = '670a310def788db39213121ddfb61762'
 process.env.LANGUAGE = 'en'
+process.env.WEATHER_TOKEN = 'a5182a087aa8225f7a9ce1c28acca2fb'
 // Write your own configuration here:
 // ...
